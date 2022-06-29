0fd4e8ab1829dc0c044fbbdd78e3757a2fac96ea
richardMRa <richete05@gmail.com>
2022-06-29 16:30:50 -0500

    [add:] navigate controls

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import { useRef } from "react";
 import { useEffect } from "react";
 import './calendar.css'
 
 const Calendar = () => {
     const monthDate = {
-        lang: 'en-ES',
+        lang: 'en-US',
         options: {
             month: 'long'
         }
@@ -32,70 +33,118 @@ const Calendar = () => {
         "Friday",
         "Saturday"
     ]
+    const [today, setToday] = useState(new Date())
     const [date, setDate] = useState(new Date())
-    const [state, setState] = useState(
-        {
+    const [state, setState] = useState(null)
+
+    const ifDatesAreEqual = () => {
+        if (state) {
+            if (today.toLocaleDateString('default', {
+                year: 'numeric',
+                month: 'long'
+            }) === state.selectedDate.toLocaleDateString('default', {
+                year: 'numeric',
+                month: 'long'
+            })) {
+                return true
+            }
+        }
+        return false
+    }
+
+    const handleNextMonth = () => {   
+        setDate(new Date(date.getFullYear(), date.getMonth() + 1, '1'))
+    }
+
+    const handlePrevMonth = () => {
+        if (ifDatesAreEqual()) {
+            return
+        }
+        setDate(new Date(date.getFullYear(), date.getMonth() - 1, '1'))
+    }
+
+    const handleMonthSelect = () =>{
+
+    }
+    
+    useEffect(() => {
+    }, [state])
+    useEffect(() => {
+        setState({
             firstDay: new Date(date.getFullYear(), date.getMonth(), '1').getDay(),
-            selectedDate: '',
+            selectedDate: new Date(date.getFullYear(), date.getMonth(), date.getDay()),
             selectedYear: date.getFullYear(),
             selectedMonth: date.toLocaleDateString(monthDate.lang, monthDate.options),
-            selectedDay: date.getDate(), 
-        }
-    )
+            selectedDay: date.getDay(),
 
-    useEffect(() => {
-        
-    })
+        })
+
+    }, [date])
 
     return (
         <div>
-            <div className="calendar-main-container">
-                <div className="calendar-header">
-                    <div className="calendar-controls">
-                        <div className="calendar-prev-button">
-                            <i className="fa-solid fa-chevron-left"></i>
+            {state ? (
+                <div className="calendar-main-container">
+                    <div className="calendar-header">
+                        <div className="calendar-controls">
+                            <div className={
+                                `calendar-prev-button 
+                                ${today.toLocaleDateString('default', {
+                                    year: 'numeric',
+                                    month: 'long'
+                                }) === state.selectedDate.toLocaleDateString('default', {
+                                    year: 'numeric',
+                                    month: 'long'
+                                }) ? '' : 'calendar-prev-button-active'}`
+                            }
+                                onClick={handlePrevMonth}>
+                                <i className="fa-solid fa-chevron-left"></i>
+                            </div>
+                            <div className="calendar-header-label">
+                                <div className="calendar-month" onClick={handleMonthSelect}>{state.selectedMonth}</div>
+                                <div className="calendar-year">{state.selectedYear}</div>
+                            </div>
+                            <div className="calendar-next-button" onClick={handleNextMonth}>
+                                <i className="fa-solid fa-chevron-right"></i>
+                            </div>
                         </div>
-                        <div className="calendar-header-label">
-                            <div className="calendar-month">{state.selectedMonth}</div>
-                            <div className="calendar-year">{state.selectedYear}</div>
-                        </div>
-                        <div className="calendar-next-button">
-                            <i className="fa-solid fa-chevron-right"></i>
+                    </div>
+                    <div className="calendar-body">
+                        <div className="calendar-days-main-container">
+                            {
+                                arrDays.map((weekDay) => {
+                                    return (
+                                        <div className="calendar-body-weekday">{weekDay.slice(0, 3)}</div>
+                                    )
+                                })
+                            }
+                            {
+                                /*Days offset*/
+                                Array(state.firstDay).fill().map((_, index) => {
+                                    return (
+                                        <div className="calendar-day-container" key={'calendar-day-offset' + index}></div>
+                                    )
+
+                                })
+                            }
+                            { /* Day tiles*/
+                                Array(arrMonth[state.selectedMonth]).fill().map((_, index) => {
+                                    return (
+                                        <div className="calendar-day-container">
+                                            <div key={'calendar-day-key' + index}>{index + 1}</div>
+                                        </div>
+                                    )
+
+                                })
+                            }
                         </div>
                     </div>
                 </div>
-                <div className="calendar-body">
-                    <div className="calendar-days-main-container">
-                        {
-                            arrDays.map((weekDay) => {
-                                return (
-                                    <div className="calendar-body-weekday">{weekDay.slice(0, 3)}</div>
-                                )
-                            })
-                        }
-                        {
-                            /*Days offset*/
-                            Array(state.firstDay).fill().map((_, index) => {
-                                return (
-                                    <div className="calendar-day-container" key={'calendar-day-offset' + index}></div>
-                                )
+            ) : (
+                <div> cargando...</div>
+            )
+            }
 
-                            })
-                        }
-                        { /* Day tiles*/
-                            Array(arrMonth[state.selectedMonth]).fill().map((_, index) => {
-                                return (
-                                    <div className="calendar-day-container">
-                                        <div key={'calendar-day-key' + index}>{index + 1}</div>
-                                    </div>
-                                )
-
-                            })
-                        }
-                    </div>
-                </div>
-
-            </div>
         </div>
     )
 }
